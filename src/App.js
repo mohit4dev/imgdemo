0582f1c6881fb0d87455d63ee1b8cff3ffac7049
msmtr14 <mohitsinghmlp@gmail.com>
2020-04-13 20:26:44 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+const images = [
+  "https://www.esa.int/var/esa/storage/images/esa_multimedia/videos/2018/05/mars_sample_return/17493376-1-eng-GB/Mars_sample_return_pillars.jpg",
+  "https://bellard.org/bpg/2.png",
+  "https://software-carpentry.org/files/2014/01/novice-sample.png",
+  "https://pngimage.net/wp-content/uploads/2018/06/sample-png-images-5.png",
+];
+export default class App extends Component {
+  state = {
+    img: images[0],
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  render() {
+    return (
+      <div
+        className="App"
+        style={{
+          flex: 1,
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <div
+          style={{
+            width: 700,
+            height: 400,
+            alignSelf: "center",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <img
+            style={{
+              width: "auto",
+              height: 400,
+            }}
+            src={this.state.img}
+            alt=""
+          />
+        </div>
+        <div
+          style={{
+            width: "100%",
+            marginTop: 50,
+            display: "flex",
+            alignSelf: "center",
+            flexDirection: "row",
+          }}
+        >
+          {images.map((img, i) => (
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <img
+                key={i}
+                style={{
+                  width: "auto",
+                  height: 100,
+                  marginRight: 20,
+                  marginBottom: 20,
+                }}
+                src={img}
+                alt=""
+              />
+              <button onClick={() => this.setState({ img })}>Show Me!</button>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
